718dcec303e8524272e3f013d233599387dd5127
daniel_cohen <daniel1cohen13579@gmail.com>
2023-12-27 18:23:37 +0200

    add section contact and animation and make more responsive

--- js/script.js
+++ js/script.js
@@ -40,3 +40,25 @@ window.onscroll = () => {
     navbar.classList.remove('active')
 };
 
+/*================= scroll reveal =================*/
+ScrollReveal({
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', {origin:'top'})
+ScrollReveal().reveal('.home-img, .project-box', {origin:'bottom'})
+ScrollReveal().reveal('.home-content h1, .about-img', {origin:'left'})
+ScrollReveal().reveal('.home-content p, .chatBot', {origin:'right'})
+
+/*================= typed =================*/
+const typed = new Typed('.multiple-text',{
+    strings: ['Software engineering student'],
+    typeSpeed: 100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+
+});
